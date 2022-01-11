2a3de53208afb465ffab200d923d3cb48118311b
akin-altrio <65419110+akin-altrio@users.noreply.github.com>
2022-01-10 22:53:17 -0500

    app update

--- app.py
+++ app.py
@@ -1,9 +1,50 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
+
+# Set up database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect database into classes
+Base = automap_base()
+
+# Reflect tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Set up Flask
 app = Flask(__name__)
 
 # create flask routes
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello world'
+def welcome():
+    return(
+        '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+
+# to run use the following the terminal
+# export FLASK_APP=app.py
+# python3 -m flask run
